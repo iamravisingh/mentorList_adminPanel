2a3278ed9d671e20bad3842ed34ae8c56a0e8ef7
Ravi Singh <ravi.singh@adapty.com>
2020-01-12 16:30:03 +0530

    add procfile to deploy for heroku

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.engine(
 );
 app.set("view engine", "hbs");
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server connected successfully at port 3000");
 });
 
